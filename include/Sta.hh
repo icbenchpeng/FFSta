13f327885777178436cda41c59a64cb4435a8a0f
peng <pengd@icbench.com>
2024-04-08 18:19:01 +0800

    add files

---
+++ include/Sta.hh
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "sta/Fuzzy.hh"
+#include "sta/Graph.hh"
+#include "sta/GraphClass.hh"
+#include "sta/Tag.hh"
+#include "sta/Search.hh"
+#include "sta/TagGroup.hh"
+#include "sta/PathAnalysisPt.hh"
