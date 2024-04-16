44f8ba103f47dfcf0c82e83837e7916c67950580
peng <pengd@icbench.com>
2024-04-16 16:58:19 +0800

    add sta dep

--- include/Sta.hh
+++ include/Sta.hh
@@ -7,3 +7,25 @@
 #include "sta/Search.hh"
 #include "sta/TagGroup.hh"
 #include "sta/PathAnalysisPt.hh"
+#include "sta/Bfs.hh"
+#include "sta/Liberty.hh"
+#include "sta/LibertyClass.hh"
+
+namespace fsta {
+
+using StaState = sta::StaState;
+using Vertex   = sta::Vertex;
+using Edge     = sta::Edge;
+using Level    = sta::Level;
+
+using BfsIndex             = sta::BfsIndex;
+using BfsBkwdIterator      = sta::BfsBkwdIterator;
+using SearchPred           = sta::SearchPred;
+using SearchPredNonReg2    = sta::SearchPredNonReg2;
+using VertexInEdgeIterator = sta::VertexInEdgeIterator;
+
+using LibertyCell = sta::LibertyCell;
+using LibertyPort = sta::LibertyPort;
+using VertexSet   = sta::VertexSet;
+
+} // end namespace fsta
